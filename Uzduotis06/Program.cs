5430e62e2cb103dd2e7d42357f99023c2dc7b1cc
EvaldasLek <evka.lt@gmail.com>
2023-10-29 22:13:59 +0200

    Ad Uzduotis06

---
+++ Uzduotis06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Uzduotis06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /* Parašyti Mano vardas tarp eilučių
+             */
+            string startLine = "------------------------";
+            string emptyLine = "|                       |";
+            string name = "E V A L D A S";
+            Console.WriteLine(startLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine("|       Mano vardas     |");
+            Console.WriteLine("|      " + name + "    |");
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(startLine);
+            Console.WriteLine();
+        }
+    }
+}
