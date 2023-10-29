285618962508112a4ca9f73ac8de54461bd9d5a2
EvaldasLek <evka.lt@gmail.com>
2023-10-29 21:16:51 +0200

    Ad Uzduotis04

---
+++ Užduotis04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Užduotis04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /* Susikurkite skaičiaus kintamąjį, 
+             * kurį išveskite penkis kartus toje pačioje eilutėje
+             * be tarpų tarp šių skaičių 
+             * (skaičius - 25, išvedimas - 2525252525).
+             */
+
+            int number = 25;
+            Console.Write(number);
+            Console.Write(number);
+            Console.Write(number);
+            Console.Write(number);
+            Console.WriteLine(number);   
+        }
+    }
+}
