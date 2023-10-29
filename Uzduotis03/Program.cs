686b237bd251b5f631e5178f9df8de46c2df5b45
EvaldasLek <evka.lt@gmail.com>
2023-10-29 18:16:57 +0200

    Puch Uzduotis03

---
+++ Uzduotis03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /*
+             * Susikurkite kintamąjį žmogaus amžiui nusakyti, 
+             * išveskite į ekraną tekstą su šiuo amžiumi: 
+             * “Įvestas amžius: {amžius}”. 
+             * Tarp {} nurodytas kintamasis, 
+             * kurio reikšmė toje vietoje turi būti įstatyta.
+             */
+            int age = 48;
+            Console.WriteLine("Įvestas amžius");
+            Console.WriteLine(48);
+        }
+    }
+}
